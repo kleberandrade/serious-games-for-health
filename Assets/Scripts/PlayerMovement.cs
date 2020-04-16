e20e785959b87923c955d1ad8d3b42a0a9b068d9
lanaschimitez <lana.schimitez@gmail.com>
2020-04-15 23:15:53 -0300

    Mudanças Joystick
    Mudanças no script do movimento do player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,7 @@ using UnityEditor;
 public class PlayerMovement : MonoBehaviour
 {
     public Joystick joystick;
-    public float speed = 2.0f;
-
-    // Pathfinding
-    Vector3[] path;
-    int targetIndex;
+    public float speed = 2f;
 
     private Rigidbody2D rb2d;
 
@@ -18,52 +14,32 @@ public class PlayerMovement : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
-
     void Update()
     {
-        // Pathfinding
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 origin = transform.position;
-            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.z = 0;
-
-            PathRequestManager.RequestPath(origin, target, OnPathFound);
-        }
-
         Vector2 movement = Vector2.zero;
 
         float moveHorizontal = joystick.Horizontal;
         float moveVertical = joystick.Vertical;
 
         movement = new Vector2(moveHorizontal, moveVertical);
-
+        if (moveVertical > 0)
+        {
+            moveVertical = 1;
+            Debug.Log("Vertical" + moveVertical);
+        }
+        if(moveVertical < 0)
+        {
+            moveVertical = -1;
+        }
+        if(moveHorizontal > 0)
+        {
+            moveHorizontal = 1;
+        }
+        if(moveHorizontal < 0)
+        {
+            moveHorizontal = -1;
+        }
+        Debug.Log("Normalize" + movement.normalized);
         rb2d.MovePosition(rb2d.position + movement.normalized * speed * Time.deltaTime);
-    }
-
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful) {
-        if (pathSuccessful) {
-            path = newPath;
-            StopCoroutine("FollowPath");
-            if (path.Length <= 0) return;
-            StartCoroutine("FollowPath");
-        }
-    }
-
-    IEnumerator FollowPath() {
-        Vector3 currentWaypoint = path[0];
-
-        while (true) {
-            if (transform.position == currentWaypoint) {
-                targetIndex++;
-                if (targetIndex >= path.Length) {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
-            yield return null;
-
-        }
     }
 }
