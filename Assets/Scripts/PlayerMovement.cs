5f713347f9b4045a74e7b91f717355fcb63267b1
lanaschimitez <lana.schimitez@gmail.com>
2020-04-17 10:32:46 -0300

    Animações parciais
    Animacoes antes de trocar o joystick

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,17 @@ using UnityEditor;
 public class PlayerMovement : MonoBehaviour
 {
     public Joystick joystick;
-    public float speed = 2f;
+    public float speed = 7f;
+    Animator animator;
+    public float angle;
+
 
     private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
     void Update()
     {
@@ -20,26 +24,11 @@ public class PlayerMovement : MonoBehaviour
 
         float moveHorizontal = joystick.Horizontal;
         float moveVertical = joystick.Vertical;
-
+        angle = Mathf.Atan2(moveHorizontal, moveVertical) * Mathf.Rad2Deg; //criando uma variavel de angulo
+        angle = (angle + 360) % 360;
+        Debug.Log(angle);
+        animator.SetFloat("Angle", angle);
         movement = new Vector2(moveHorizontal, moveVertical);
-        if (moveVertical > 0)
-        {
-            moveVertical = 1;
-            Debug.Log("Vertical" + moveVertical);
-        }
-        if(moveVertical < 0)
-        {
-            moveVertical = -1;
-        }
-        if(moveHorizontal > 0)
-        {
-            moveHorizontal = 1;
-        }
-        if(moveHorizontal < 0)
-        {
-            moveHorizontal = -1;
-        }
-        Debug.Log("Normalize" + movement.normalized);
         rb2d.MovePosition(rb2d.position + movement.normalized * speed * Time.deltaTime);
     }
 }
