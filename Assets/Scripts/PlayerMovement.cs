abb96a9eaa0181f7b7a67fdf9d7966e402bb8fe4
lanaschimitez <lana.schimitez@gmail.com>
2020-04-17 13:04:04 -0300

    Finalização das animações

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 7f;
     Animator animator;
     public float angle;
-
+    public bool isRun;
+    public int direction;
 
     private Rigidbody2D rb2d;
 
@@ -26,28 +27,40 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = joystick.Vertical;
         angle = Mathf.Atan2(moveHorizontal, moveVertical) * Mathf.Rad2Deg; //criando uma variavel de angulo
         angle = (angle + 360) % 360;
-        Debug.Log(angle);
-        animator.SetFloat("Angle", angle);
         if (angle >= 135  && angle < 225) //Down
         { 
             moveHorizontal = 0;
             moveVertical = -1;
-        }
+            direction = 1;
+}
         if (angle >= 225 && angle < 315) //Left
         {
             moveHorizontal = -1;
             moveVertical = 0;
+            direction = 2;
         }
         if (angle >=315 || angle > 0 && angle < 45) //Up
         {
             moveHorizontal = 0;
             moveVertical = 1;
+            direction = 3;
         }
         if (angle >=45 && angle < 135) // Rigth
         {
             moveHorizontal = 1;
             moveVertical = 0;
+            direction = 4;
         }
+        if (moveVertical == 0 && moveHorizontal == 0)
+        {
+            isRun = false;
+        }
+        else
+        {
+            isRun = true;
+        }
+        animator.SetBool("IsRun", isRun);
+        animator.SetInteger("Direction", direction);
         movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.MovePosition(rb2d.position + movement.normalized * speed * Time.deltaTime);
     }
