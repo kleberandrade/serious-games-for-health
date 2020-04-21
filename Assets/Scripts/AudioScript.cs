4086ba8413617a211639aa2f5e3c6b1b19e5bb7d
dantasleonardo <47763070+dantasleonardo@users.noreply.github.com>
2020-04-20 22:35:38 -0300

    Audio e level

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    private static AudioScript instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+
+}
