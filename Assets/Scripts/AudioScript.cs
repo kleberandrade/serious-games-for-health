d94e353dca704a71e49bda70434b0784c1991060
lanaschimitez <lana.schimitez@gmail.com>
2020-04-22 22:10:00 -0300

    Correcao no som

--- Assets/Scripts/AudioScript.cs
+++ Assets/Scripts/AudioScript.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioScript : MonoBehaviour
 {
     private static AudioScript instance;
-
+   
     void Awake()
     {
         if (instance != null)
