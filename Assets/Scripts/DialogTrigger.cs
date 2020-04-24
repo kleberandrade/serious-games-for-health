ac695f3411e79dfb757a3ae2536e1b1f4601fbef
lanaschimitez <lana.schimitez@gmail.com>
2020-04-24 19:37:45 -0300

    Controle final

--- Assets/Scripts/DialogTrigger.cs
+++ Assets/Scripts/DialogTrigger.cs
@@ -7,6 +7,22 @@ public class DialogTrigger : MonoBehaviour
     public Dialog m_Dialog;
     public bool talked = false;
     public bool unlock;
+    public GameObject controleFinal;
+    private float countdown = 3.0f;
+
+    public void Update()
+    {
+        if (gameObject.name == "Nurse" && talked)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 1)
+            {
+                controleFinal.SetActive(true);
+                countdown = 0;
+            }
+
+        }
+    }
     public void ToggleDialog()
     {
         if (unlock == true)
